e078ab139ff826110140ff58c13a409d37c8a4e8
Jason Fritcher <jkf@wolfnet.org>
2023-11-17 22:13:48 -0600

    Implement decoding strings

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,49 @@
+use anyhow::{anyhow, Result};
 use serde_json;
 use std::env;
 
 // Available if you need it!
 // use serde_bencode
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value> {
+    match encoded_value.chars().next().unwrap() {
+        // If encoded_value starts with a digit, it's a number
+        '0'..='9' => {
+            // Example: "5:hello" -> "hello"
+            let colon_index = encoded_value.find(':').unwrap();
+            let number_string = &encoded_value[..colon_index];
+            let number = number_string.parse::<i64>()?;
+            let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+            return Ok(serde_json::Value::String(string.to_string()));
+        }
+        _ => {
+            return Err(anyhow!("Unhandled encoded value: {}", encoded_value));
+        }
     }
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main() {
+fn main() -> Result<()>{
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
-    if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
+    match command.as_str() {
+        "decode" => {
+            let encoded_value = &args[2];
+            let decoded_value = decode_bencoded_value(encoded_value)?;
+            println!("{}", decoded_value.to_string());
+        }
+        _ => { println!("unknown command: {}", command) }
+    }
+    return Ok(());
+}
 
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decode_bencoded_value() {
+        assert_eq!(decode_bencoded_value("5:hello").unwrap(), serde_json::Value::String("hello".to_string()));
     }
 }
