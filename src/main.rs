7169710dab5eb7ff9f3289f3efa8a285dc3eb6ab
Jason Fritcher <jkf@wolfnet.org>
2023-11-19 19:12:24 -0600

    Implement peer discovery

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,53 @@
 #[allow(unused_imports)]
 use anyhow::{anyhow, Result};
+use reqwest;
 use serde_bencode;
 use sha1::Digest;
-use std::{env, fs};
+use std::{env, fs, net::SocketAddrV4};
 
 mod decoder;
 mod types;
 
 use crate::decoder::decode_bencoded_value;
-use crate::types::{Files, Info, Torrent};
+use crate::types::{Files, Info, Torrent, TrackerResponse};
 
 fn calculate_info_hash(info: &Info) -> Result<[u8; 20]> {
     let encoded_info = serde_bencode::to_bytes(info)?;
     let mut hasher = sha1::Sha1::new();
     hasher.update(&encoded_info);
     return Ok(hasher.finalize().into());
+}
+
+fn urlencode_info_hash(info_hash: &[u8; 20]) -> String {
+    let mut escaped_info_hash = String::with_capacity(info_hash.len() * 3);
+    for byte in info_hash {
+        escaped_info_hash.push_str(format!("%{:02X}", byte).as_str());
+    }
+    return escaped_info_hash;
+}
+
+fn get_peers_from_tracker(announce_url: String, info_hash: &[u8; 20], left: usize) -> Result<Vec<SocketAddrV4>> {
+    // Build url with query parameters
+    let mut url = announce_url;
+    if url.contains("?") {
+        url.push_str("&");
+    } else {
+        url.push_str("?");
+    }
+    url.push_str(format!("info_hash={}", urlencode_info_hash(info_hash)).as_str());
+    url.push_str("&peer_id=00112233445566778899");
+    url.push_str("&port=6881");
+    url.push_str("&uploaded=0");
+    url.push_str("&downloaded=0");
+    url.push_str(format!("&left={}", left).as_str());
+    url.push_str("&compact=1");
+
+    println!("Url: {}", url);
+
+    let response = reqwest::blocking::get(&url)?.bytes()?;
+    let response: TrackerResponse = serde_bencode::from_bytes(&response)?;
+
+    return Ok(response.peers.0);
 }
 
 fn main() -> Result<()> {
@@ -44,6 +77,20 @@ fn main() -> Result<()> {
             println!("Piece Hashes:");
             for hash in torrent.info.pieces.0 {
                 println!("{}", hex::encode(hash));
+            }
+        }
+        "peers" => {
+            let torrent_name = &args[2];
+            let encoded_value = fs::read(torrent_name)?;
+            let torrent: Torrent = serde_bencode::from_bytes(&encoded_value)?;
+            let info_hash = calculate_info_hash(&torrent.info)?;
+            let left = match torrent.info.files {
+                Files::Single { length } => length,
+                Files::Multiple { files } => files.iter().map(|file| file.length).sum(),
+            };
+            let peers = get_peers_from_tracker(torrent.announce, &info_hash, left)?;
+            for peer in peers {
+                println!("{}:{}", peer.ip(), peer.port());
             }
         }
         _ => { println!("unknown command: {}", command) }
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use hashes::Hashes;
+use peers::Peers;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Torrent {
@@ -28,6 +29,12 @@ pub enum Files {
 pub struct File {
     pub length: usize,
     pub path: Vec<String>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct TrackerResponse {
+    pub interval: usize,
+    pub peers: Peers,
 }
 
 mod hashes {
@@ -77,6 +84,52 @@ mod hashes {
                 v.chunks(20)
                     .map(|chunk| {
                         chunk.try_into().expect("chunk is always 20 bytes")
+                    })
+                    .collect(),
+            ))
+        }
+    }
+}
+
+mod peers {
+    use serde::{Deserialize, Deserializer, de::Visitor};
+    use std::{fmt, net::{Ipv4Addr, SocketAddrV4}};
+
+    #[derive(Debug, Clone)]
+    pub struct Peers(pub Vec<SocketAddrV4>);
+    impl<'de> Deserialize<'de> for Peers {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            deserializer.deserialize_bytes(PeersVisitor)
+        }
+    }
+
+    struct PeersVisitor;
+    impl<'de> Visitor<'de> for PeersVisitor {
+        type Value = Peers;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a byte string with a length divisible by 6")
+        }
+
+        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            if v.len() % 6 != 0 {
+                return Err(E::custom(format!(
+                    "expected a byte string with a length divisible by 6, got {}",
+                    v.len()
+                )));
+            }
+            Ok(Peers(
+                v.chunks(6)
+                    .map(|chunk| {
+                        let ip = Ipv4Addr::new(chunk[0], chunk[1], chunk[2], chunk[3]);
+                        let port = u16::from_be_bytes([chunk[4], chunk[5]]);
+                        SocketAddrV4::new(ip, port)
                     })
                     .collect(),
             ))
